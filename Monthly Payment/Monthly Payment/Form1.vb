4cee4821fb3b5448f9522559bcce2b80fa4b6a32
Monicah <moniquenyakundi@gmail.com>
2023-06-04 23:03:36 +0300

    a simple visual basic project.

---
+++ Monthly Payment/Monthly Payment/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub lblCalculate_Click(sender As Object, e As EventArgs) Handles lblCalculate.Click
+        Dim dblPay As Double
+        lblPay.Text = String.Empty
+        dblPay = -Financial.Pmt(0.05 / 12, 3 * 12, 50000)
+        lblPay.Text = dblPay.ToString("C2")
+
+    End Sub
+End Class
